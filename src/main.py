42ba81ace83eed303b277554b071d33ab7239b3d
Danjfreire <danjfreire@gmail.com>
2025-09-06 09:57:02 -0300

    refactor: refactored url repo to use a clas

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel,HttpUrl 
 from shortener import generate_random_code
-from url_repo import save_shortened_url, find_by_original_url, find_by_code, update_shortened_url, delete_shortened_url 
+from url_repo import UrlRepo
 
 app = FastAPI()
 
 class UrlDTO(BaseModel):
     url: HttpUrl
 
+url_repo = UrlRepo()
+
 
 @app.post("/shorten", status_code=status.HTTP_201_CREATED)
 def shorten_url(body: UrlDTO):
-    short_url = find_by_original_url(body.url)
+    short_url = url_repo.find_by_original_url(body.url)
 
     if short_url:
         return short_url
@@ -20,33 +22,43 @@ def shorten_url(body: UrlDTO):
     if code is None:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="could not generate short code")
 
-    short_url = save_shortened_url(body.url, code)
+    short_url = url_repo.save_shortened_url(body.url, code)
     return short_url
 
 @app.get("/shorten/{code}")
 def get_original_url(code: str):
-    short_url = find_by_code(code)
+    short_url = url_repo.find_by_code(code)
 
     if short_url is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"url not found for code {code}")
     
+    url_repo.increase_stats(code)
     return short_url
 
 @app.put("/shorten/{code}")
 def update_short_url(code: str, body: UrlDTO):
-    short_url = find_by_code(code)
+    short_url = url_repo.find_by_code(code)
 
     if short_url is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"url not found for code {code}")
     
-    return update_shortened_url(short_url, body.url)
+    return url_repo.update_shortened_url(short_url, body.url)
 
 @app.delete("/shorten/{code}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_short_url(code: str):
-    short_url = find_by_code(code)
+    short_url = url_repo.find_by_code(code)
 
     if short_url is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"url not found for code {code}")
     
-    delete_shortened_url(short_url)
+    url_repo.delete_shortened_url(short_url)
+    
+@app.get("/shorte/{code}/stats")
+def get_code_stats(code: str):
+    short_url = url_repo.find_by_code(code)
+
+    if short_url is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"url not found for code {code}")
+    
+    return url_repo.find_shortened_url_stats(short_url) 
     
--- src/url_repo.py
+++ src/url_repo.py
@@ -8,42 +8,63 @@ class ShortenedUrl(BaseModel):
     created_at: datetime.datetime
     updated_at: datetime.datetime
 
-url_db: Dict[str, ShortenedUrl ] = {}
-code_db: Dict[str, ShortenedUrl] = {}
+class ShortnedUrlWithAccess(ShortenedUrl):
+    access_count: int
 
+class UrlRepo:
+    def __init__(self):
+        self.url_db: Dict[str, ShortenedUrl] = {}
+        self.code_db: Dict[str, ShortenedUrl] = {}
+        self.stats_db: Dict[str, int] = {}
 
-def find_by_original_url(url: str) -> ShortenedUrl | None:
-    if url in url_db: 
-        return url_db[url]
-    return None
+    def find_by_original_url(self, url: str) -> ShortenedUrl | None:
+        if url in self.url_db: 
+            return self.url_db[url]
+        return None
 
-def find_by_code(code : str) -> ShortenedUrl | None:
-    if code in code_db:
-        return code_db[code]
-    return None
+    def find_by_code(self, code : str) -> ShortenedUrl | None:
+        if code in self.code_db:
+            return self.code_db[code]
+        return None
 
-def save_shortened_url(url: str, code: str) -> ShortenedUrl:
-    now = datetime.datetime.now()
-    short: ShortenedUrl = ShortenedUrl(url=url, code=code, created_at=now, updated_at=now) 
-    url_db[url] = short
-    code_db[code] = short
-    return short 
+    def save_shortened_url(self, url: str, code: str) -> ShortenedUrl:
+        now = datetime.datetime.now()
+        short: ShortenedUrl = ShortenedUrl(url=url, code=code, created_at=now, updated_at=now) 
+        self.url_db[url] = short
+        self.code_db[code] = short
+        return short 
 
-def update_shortened_url(shortened_url: ShortenedUrl, new_url: HttpUrl):
-    now = datetime.datetime.now()
-    old_url = shortened_url.url
+    def update_shortened_url(self, shortened_url: ShortenedUrl, new_url: HttpUrl):
+        now = datetime.datetime.now()
+        old_url = shortened_url.url
 
-    shortened_url.url = new_url
-    shortened_url.updated_at = now
+        shortened_url.url = new_url
+        shortened_url.updated_at = now
 
-    url_db.pop(old_url)
-    url_db[new_url] = shortened_url
-    code_db[shortened_url.code] = shortened_url
+        self.url_db.pop(old_url)
+        self.url_db[new_url] = shortened_url
+        self.code_db[shortened_url.code] = shortened_url
 
-    return shortened_url
+        return shortened_url
 
-def delete_shortened_url(short_url: ShortenedUrl):
-    url_db.pop(short_url.url)
-    code_db.pop(short_url.code)
+    def delete_shortened_url(self,short_url: ShortenedUrl):
+        self.url_db.pop(short_url.url)
+        self.code_db.pop(short_url.code)
+
+    def increase_stats(self, code: str, increase: int = 1):
+        if not code in self.stats_db:
+            self.stats_db[code] = 0
+        
+        self.stats_db[code] += increase
+
+    def find_shortened_url_stats(self,short_url: ShortenedUrl):
+        access_count = self.stats_db.get(short_url.code, 0)
+        return ShortnedUrlWithAccess(
+            url=short_url.url,
+            code=short_url.code, 
+            created_at=short_url.created_at,
+            updated_at=short_url.updated_at, 
+            access_count=access_count)
+
 
 
